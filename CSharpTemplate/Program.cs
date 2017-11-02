22a0ed1450c4a7ff9ec4ea1256d648e067725837
m <abc-0-4@yandex.ru>
2017-11-02 19:10:26 +0300

    Проект для создания ботов

---
+++ CSharpTemplate/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTemplate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SolverA.Run();
+           // SolverB.Run();
+           // SolverC.Run();
+           // SolverD.Run();
+           // SolverE.Run();
+        }
+    }
+}
