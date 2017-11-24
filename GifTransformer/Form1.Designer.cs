32889d42c30727899638a09c8057808598447b76
Mikhail <abc-0-4@yandex.ru>
2017-11-24 05:54:49 +0300

    Добавил проект GifTransformer

---
+++ GifTransformer/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace GifTransformer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(49, 33);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(70, 124);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(569, 334);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "64",
+            "128",
+            "256",
+            "512",
+            "1024",
+            "2048"});
+            this.comboBox1.Location = new System.Drawing.Point(238, 35);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.Text = "1024";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(151, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "TOTAL SIZE = ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(759, 491);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ GifTransformer/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.Drawing.Drawing2D;
+
+namespace GifTransformer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var fileDialog = new OpenFileDialog { Filter = "GIF|*.gif" };
+            if (fileDialog.ShowDialog() != DialogResult.OK)
+                return;
+
+            Image gif = Image.FromFile(fileDialog.FileName);
+            FrameDimension dim = new FrameDimension(gif.FrameDimensionsList[0]);
+            int frames = gif.GetFrameCount(dim);
+            int framesInRow = Enumerable.Range(1, frames).First(x => x * x >= frames);
+            Bitmap resultingImage = new Bitmap(gif.Width * 6, gif.Height*5);
+
+            for (int i = 0; i < frames; i++)
+            {
+                gif.SelectActiveFrame(dim, i);
+                
+                Rectangle destRegion = new Rectangle(gif.Width * (i% framesInRow), gif.Height*(i/ framesInRow), gif.Width, gif.Height);
+                Rectangle srcRegion = new Rectangle(0, 0, gif.Width, gif.Height);
+
+                using (Graphics grD = Graphics.FromImage(resultingImage))
+                {
+                    grD.CompositingMode = CompositingMode.SourceCopy;
+                    grD.CompositingQuality = CompositingQuality.HighQuality;
+                    grD.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                    grD.SmoothingMode = SmoothingMode.HighQuality;
+                    grD.PixelOffsetMode = PixelOffsetMode.HighQuality;
+                    var bmp = new Bitmap(gif);
+                    for (int row = 0; row < gif.Height; row++)
+                    {
+                        for (int col = 0; col < gif.Width; col++)
+                        {
+                            var pixel = bmp.GetPixel(col, row);
+                            bmp.SetPixel(col,row, TranslatePixel(pixel, col, row, gif.Width, gif.Height));
+                        }
+                    }
+                    grD.DrawImage(bmp, destRegion, srcRegion, GraphicsUnit.Pixel);
+
+                   // var g = Graphics.FromImage(gif);
+                   // g.DrawImage(bmp, new Point());
+                }
+
+            }
+
+
+
+            pictureBox1.Image = resultingImage;
+            resultingImage = ResizeImage(resultingImage, int.Parse(comboBox1.Text), int.Parse(comboBox1.Text));
+
+
+            resultingImage.Save( fileDialog.FileName.Substring(0,fileDialog.FileName.Length-3)+"png", ImageFormat.Png);
+            MessageBox.Show("Сохранено в той же папке. Кадров по горизонтали: " + framesInRow + ", по вертикали: " + Math.Ceiling(frames * 1.0 / framesInRow)+". Исходный размер: "+gif.Width+" x "+gif.Height);
+        }
+
+        Color TranslatePixel(Color pixel, int x, int y, int width, int height)
+        {
+            double close = 0.1;
+
+            double hor = (double)x/width, vert = (double)y/height;
+
+            if (x > width / 2) hor = 1 - hor;
+            if (y > height / 2) vert = 1 - vert;
+
+            var realClose = Math.Min(hor, vert);
+            if (realClose < 0) realClose = 0;
+
+            double mult = realClose < close ? (realClose / close ) : 0.9999999;
+
+            if(pixel.B + pixel.R + pixel.G > 200)
+                return Color.FromArgb((int)(254*(mult+0.0000001)), pixel);
+            double a = (pixel.B + pixel.R + pixel.G) / 200.0;
+            if (a < 0) a = 0;
+            int alpha = (int)(a* mult* 254);
+            
+            return Color.FromArgb(alpha, pixel);
+
+        }
+
+        public static Bitmap ResizeImage(Image image, int width, int height)
+        {
+            var destRect = new Rectangle(0, 0, width, height);
+            var destImage = new Bitmap(width, height);
+
+            destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+            using (var graphics = Graphics.FromImage(destImage))
+            {
+                graphics.CompositingMode = CompositingMode.SourceCopy;
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = SmoothingMode.HighQuality;
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                using (var wrapMode = new ImageAttributes())
+                {
+                    wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+                    graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+                }
+            }
+
+            return destImage;
+        }
+
+      //  public static Image gif = Image.FromFile("image.gif"); 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+         //   pictureBox1.Image = gif; 
+            //pictureBox1.
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //var res = gif.Clone();
+            
+        }
+    }
+}
